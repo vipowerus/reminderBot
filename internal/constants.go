52514a042f83cedc3035637e74e6c3c5e9a4fbf9
Alexey <alexey_chechushkov@dotwrk.com>
2022-12-17 21:43:52 +0700

    task 8 - server.go Refactor default message handling function in server.go

---
+++ internal/constants.go
@@ -0,0 +1,13 @@
+package constants
+
+const (
+	StartCommandMessage         = "Hello! Nice to meet you \nPlease enter the group number"
+	HelpCommandMessage          = "Someday there will be help"
+	ChangeGroupCommandMessage   = "Please add the group number to the command"
+	HasGroupMessage             = "У вас уже есть группа. Вы можете сменить ее с помощью команды '/change *****'"
+	NoScheduleFoundMessage      = "Не могу найти ваше расписание"
+	ScheduleNotAvailableMessage = "Расписание не открывается. Если вы опечатались, введите номер группы еще раз"
+	InternalErrorMessage        = "Internal error"
+	UserAddedToScheduleMessage  = "Вы добавлены на рассылку по расписанию группы "
+	IncorrectGroupNumberMessage = "Некорректный номер группы. Я умею работать с такими номерами: [0-9][0-9][0-9][0-9][0-9]"
+)
